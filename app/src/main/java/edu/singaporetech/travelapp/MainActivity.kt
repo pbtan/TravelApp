4d12924355706f8d0942c4ee706c05959282642b
doubleristretto <doubleristretto@users.noreply.github.com>
2020-01-19 19:40:22 +0800

    lab 2 skeleton files

---
+++ app/src/main/java/edu/singaporetech/travelapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package edu.singaporetech.travelapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Lab 02: Travel App
+ * Main Screen
+ */
+class MainActivity : AppCompatActivity() {
+
+    val TAG: String = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // TODO findviewbyid for the UI elements
+
+        // TODO set onClickListeners to all the buttons here
+        //  or declare the onclick method within the layout XML?
+    }
+}
